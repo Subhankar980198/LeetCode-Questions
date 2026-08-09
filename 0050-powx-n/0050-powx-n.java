f5b04d5f003dcf7b2e1e8aa1554eea618afdf7dd
Subhankar Mondal <subhankar9801@gmail.com>
2026-08-09 22:48:10 +0530

    Time: 0 ms (100%), Space: 48.2 MB (35.18%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Solution {
+    public double myPow(double x, int n) {
+        double res=0;
+        double pow=Math.pow(x,n);
+        
+                    return pow;
+
+    }
+}
